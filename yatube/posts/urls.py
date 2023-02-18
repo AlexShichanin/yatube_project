1dc36d501b58c921f569b5230c51b71996135f62
Алексанр Шичанин <9shichanin@gmail.com>
2023-02-18 17:24:35 +0300

    Завершенный проект

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('group/<slug:slug>/', views.group_posts),
-    path('', views.index),
-    
-]
+app_name = 'posts'
 
+urlpatterns = [
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('', views.index, name='index'),
+]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
-from django.http import HttpResponse
-# Create your views here.
+
+# Главная страница сайта
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    text = 'Главная страница проекта'
+    title = 'Это главная страница проекта'
+    context = {
+        'posts': posts,
+        'text': text,
+        'title1': title,
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    return HttpResponse(f'Отфильтрованные посты по группам, {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Старница с постами группы '
+    text = 'Группа тайных поклонников графа'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+        'text': text,
+    }
+    return render(request, 'posts/group_list.html', context)
